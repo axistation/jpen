04526bcef6490ba07fc7c4cb5933c089a7f3b04c
axistation <axistation@gmail.com>
2022-03-16 05:42:13 -0700

    loadmore

--- src/components/WordsList.js
+++ src/components/WordsList.js
@@ -1,5 +1,5 @@
-import React, { useRef } from "react"
-import { Link } from "gatsby"
+import React, { useState, useEffect, useRef } from "react"
+// import { Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 // import slugify from "slugify"
 
@@ -8,10 +8,43 @@ import styled from "styled-components"
 const WordsList = ({ words = [] }) => {
   const myAudio = useRef(null)
 
+  // State for the list
+  const [list, setList] = useState([...words.slice(0, 10)])
+
+  // State to trigger oad more
+  const [loadMore, setLoadMore] = useState(false)
+
+  // State of whether there is more to load
+  const [hasMore, setHasMore] = useState(words.length > 10)
+
+  // Load more button click
+  const handleLoadMore = () => {
+    setLoadMore(true)
+  }
+
+  // Handle loading more articles
+  useEffect(() => {
+    if (loadMore && hasMore) {
+      const currentLength = list.length
+      const isMore = currentLength < words.length
+      const nextResults = isMore
+        ? words.slice(currentLength, currentLength + 10)
+        : []
+      setList([...list, ...nextResults])
+      setLoadMore(false)
+    }
+  }, [loadMore, hasMore]) //eslint-disable-line
+
+  //Check if there is more
+  useEffect(() => {
+    const isMore = list.length < words.length
+    setHasMore(isMore)
+  }, [list]) //eslint-disable-line
+
   return (
     <Wrapper>
       <div className="wrapper">
-        {words.map(word => {
+        {list.map(word => {
           const { id, english, japanese, romaji, image, audio } = word
           const pathToImage = getImage(image)
           const audioUrl = audio.file.url
@@ -41,6 +74,15 @@ const WordsList = ({ words = [] }) => {
           )
         })}
       </div>
+      <div className="loadmore">
+        {hasMore ? (
+          <div className="btn" onClick={handleLoadMore}>
+            Load More
+          </div>
+        ) : (
+          <div className="nomore">No more results</div>
+        )}
+      </div>
     </Wrapper>
   )
 }
@@ -64,6 +106,34 @@ const Wrapper = styled.section`
   p {
     padding-top: 0.6rem;
     font-size: 0.9rem;
+  }
+  .loadmore {
+    text-align: center;
+    padding: 2rem;
+  }
+  .btn {
+    display: inline-block;
+    outline: 0;
+    cursor: pointer;
+    padding: 5px 16px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 20px;
+    vertical-align: middle;
+    border: 1px solid;
+    border-radius: 6px;
+    color: #24292e;
+    background-color: #fafbfc;
+    border-color: #1b1f2326;
+    box-shadow: rgba(27, 31, 35, 0.04) 0px 1px 0px 0px,
+      rgba(255, 255, 255, 0.25) 0px 1px 0px 0px inset;
+    transition: 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+    transition-property: color, background-color, border-color;
+    :hover {
+      background-color: #f3f4f6;
+      border-color: #1b1f2326;
+      transition-duration: 0.1s;
+    }
   }
 `
 
